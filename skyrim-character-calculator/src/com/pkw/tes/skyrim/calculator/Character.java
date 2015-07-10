a06466208f08daf5acea8169af514ed068fc5a00
Kooper Smith <klsmith6@bsu.edu>
2015-07-10 09:44:23 -0400

    Renamed param xp to amount in addXp method of Character class to avoid hiding the field xp.

--- skyrim-character-calculator/src/com/pkw/tes/skyrim/calculator/Character.java
+++ skyrim-character-calculator/src/com/pkw/tes/skyrim/calculator/Character.java
@@ -6,44 +6,44 @@ import com.pkw.tes.skyrim.calculator.Skill.Type;
 
 public class Character {
 
-	private int xp;
-	private int level;
-	private Map<Skill.Type, Skill> skillMap;
+    private int xp;
+    private int level;
+    private Map<Skill.Type, Skill> skillMap;
 
-	public static Character create(Race race) {
-		return new Character(race);
-	}
+    public static Character create(Race race) {
+        return new Character(race);
+    }
 
-	private Character(Race race) {
-		xp = 0;
-		level = 1;
-		skillMap = race.initalSkills();
-		for (Type type : skillMap.keySet()) {
-			skillMap.get(type).addOnLevelListener(OnLevelListener.getFor(this));
-		}
-	}
+    private Character(Race race) {
+        xp = 0;
+        level = 1;
+        skillMap = race.initalSkills();
+        for (Type type : skillMap.keySet()) {
+            skillMap.get(type).addOnLevelListener(OnLevelListener.getFor(this));
+        }
+    }
 
-	public void levelUp() {
-		level++;
-	}
+    public void levelUp() {
+        level++;
+    }
 
-	public Skill getSkill(Type type) {
-		return skillMap.get(type);
-	}
+    public Skill getSkill(Type type) {
+        return skillMap.get(type);
+    }
 
-	public void addXp(int xp) {
-		this.xp += xp;
-		if (xp >= xpForNextLevel()) {
-			xp -= xpForNextLevel();
-			levelUp();
-		}
-	}
+    public void addXp(int amount) {
+        xp += amount;
+        if (amount >= xpForNextLevel()) {
+            amount -= xpForNextLevel();
+            levelUp();
+        }
+    }
 
-	public int currentXp() {
-		return xp;
-	}
+    public int currentXp() {
+        return xp;
+    }
 
-	public int xpForNextLevel() {
-		return (level + 3) * 25;
-	}
+    public int xpForNextLevel() {
+        return (level + 3) * 25;
+    }
 }
