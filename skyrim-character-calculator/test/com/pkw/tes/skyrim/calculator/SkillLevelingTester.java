f8caa5d97cf338a136a56fc721355d866da112ec
Kooper Smith <klsmith6@bsu.edu>
2015-07-10 11:29:46 -0400

    Renamed CharacterTest to SkillLevelingTester to better repsent what is going on in the class.

--- skyrim-character-calculator/test/com/pkw/tes/skyrim/calculator/SkillLevelingTester.java
+++ skyrim-character-calculator/test/com/pkw/tes/skyrim/calculator/SkillLevelingTester.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CharacterTester {
+public class SkillLevelingTester {
 
     private Character nordCharacter;
 
